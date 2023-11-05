ac45f5f1728e9b33907f6f183e9726c3af72fa62
Salome Saghirishvili <sagirishvilis@gmail.com>
2023-11-05 11:54:08 +0400

    add api data

--- app.js
+++ app.js
@@ -34,3 +34,45 @@ next.forEach((item) => {
     }
   });
 });
+
+const displayDataButton = document.getElementById("button");
+console.log(displayDataButton);
+displayDataButton.addEventListener("click", fetchData);
+
+function fetchData() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => {
+      if (!res.ok) {
+        console.log("Error");
+      } else {
+        return res.json();
+      }
+    })
+    .then((apiData) => {
+      myData(apiData);
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+    });
+}
+
+function myData(apiData) {
+  const container = document.getElementById("data");
+  apiData.forEach((data) => {
+    const divElement = document.createElement("div");
+    divElement.innerHTML = `
+  <div class="card" id="${data.id}">
+    <div class="card-image-holder">
+        <img class="card-image" src="https://source.unsplash.com/300x225/?beach" alt="beach" />
+    </div>
+    <div class="card-title" >
+        <h2>
+            ${data.title}
+        </h2>
+        <p>${data.body}</p>
+    </div>
+
+  </div> `;
+    container.appendChild(divElement);
+  });
+}
